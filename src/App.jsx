1c2aba3d8b70760184411286a069c50e486ce602
Sachith Balaka 22331A4403 <sachithbalaka@gmail.com>
2025-09-15 21:31:17 +0530

    Done With SetUp

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div className='App'>
+      <div className="windowTop">
+        <div className="wtLeft"></div>
+        <div className="wtRight"></div>
+      </div>
+      <div className="windowBot"></div>
+    </div>
+  )
+}
+export default App
