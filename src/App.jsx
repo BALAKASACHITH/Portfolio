b6cc65b5874541f1c9b6a066ce2ab09a5f08ef48
Sachith Balaka 22331A4403 <sachithbalaka@gmail.com>
2025-09-16 00:04:10 +0530

    new changes added

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,14 @@ const App = () => {
   return (
     <div className='App'>
       <div className="windowTop">
-        <div className="wtLeft"></div>
-        <div className="wtRight"></div>
+        <div className="wtLeft">
+          <div className="wtlProfile"></div>
+          <div className="wtlName"><h1>Sachith Balaka</h1></div>
+        </div>
+        <div className="wtRight">
+          <div className="wtrHome">home</div>
+          <div className="wtrA">About</div>
+        </div>
       </div>
       <div className="windowBot"></div>
     </div>
